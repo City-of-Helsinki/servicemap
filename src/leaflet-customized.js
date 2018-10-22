4f418223fcaacb25cd5395389bbef62ac052f0cd
Petri Mahanen <petri.mahanen@fraktio.fi>
2018-10-22 12:18:39 +0300

    Add override for Leaflet Path::onRemove to work around click handlers being removed

--- src/leaflet-customized.js
+++ src/leaflet-customized.js
@@ -1,4 +1,22 @@
 define(['leaflet'], function(L) {
-    L.Map.prototype._originalGetBounds = L.Map.prototype.getBounds;
-    return L;
-});
+  L.Map.prototype._originalGetBounds = L.Map.prototype.getBounds
+
+  L.Path.prototype.onRemove = function(map) {
+    map
+      .off('viewreset', this.projectLatlngs, this)
+      .off('moveend', this._updatePath, this)
+
+    if (this.options.clickable) {
+      // This override removes the following line from original method:
+      // this._map.off('click', this._onClick, this);
+      this._map.off('mousemove', this._onMouseMove, this)
+    }
+
+    this._requestUpdate()
+
+    this.fire('remove')
+    this._map = null
+  }
+
+  return L
+})
