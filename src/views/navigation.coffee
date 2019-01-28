City-of-Helsinki/servicemap

commit 182256ab50747dfec5a83b4cf29dcfda97e86aea

Fix sudden unit details content closing

src/views/navigation.coffee
@@ -197,10 +197,11 @@ define (require) ->
                         collection: new models.UnitList()
 
                 when 'search'
-                    view = new SearchResultsSummaryLayout
-                        collection: @searchResults
-                    if opts?.disableAutoFocus
-                        view.disableAutoFocus()
+                    if @searchResults.length > 0
+                        view = new SearchResultsSummaryLayout
+                            collection: @searchResults
+                        if opts?.disableAutoFocus
+                            view.disableAutoFocus()
                 when 'service-node-units'
                     view = new UnitListingView
                         model: new Backbone.Model

commit 9368b67c2d55a3e7e41c77b7ca2dbf452c439ac7

Fix problem with "no search results" result not showing
The problem was introduced in commit
182256ab50747dfec5a83b4cf29dcfda97e86aea.

Fix: we differentiate between different reasons for calling
navigationlayout.change with the value 'search', with zero
search results.

src/views/navigation.coffee
@@ -197,7 +197,10 @@ define (require) ->
                         collection: new models.UnitList()
 
                 when 'search'
-                    if @searchResults.length > 0
+                    if opts?.reason != 'userClickedSearchInputElement' or @searchResults.length > 0
+                        # We want to prevent any open UI element from
+                        # closing just because the user clicked on the
+                        # search bar.
                         view = new SearchResultsSummaryLayout
                             collection: @searchResults
                         if opts?.disableAutoFocus
@@ -307,7 +310,7 @@ define (require) ->
             @browse.show new BrowseButtonView()
 
         _expandSearch: ->
-            @_open 'search', disableAutoFocus: true
+            @_open 'search', disableAutoFocus: true, reason: 'userClickedSearchInputElement'
 
         _open: (actionType, opts) ->
             @updateClasses actionType