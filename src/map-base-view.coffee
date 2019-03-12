City-of-Helsinki/servicemap

commit b301da60ff1fcb581b9ef2a9587337777e1472e8

Show only subway stop units on high zoom

src/map-base-view.coffee
@@ -46,6 +46,7 @@ define (require) ->
             @markers = {}
             @geometries = {}
             @units = @opts.units
+            @stopUnits = @opts.stopUnits
             @transportationStops = @opts.transportationStops
             @selectedUnits = @opts.selectedUnits
             @selectedPosition = @opts.selectedPosition
@@ -58,6 +59,10 @@ define (require) ->
                 # Triggered when all of the
                 # pages of units have been fetched.
                 @drawUnits @units, options
+                if @selectedUnits.isSet()
+                    @highlightSelectedUnit @selectedUnits.first()
+            @listenTo @stopUnits, 'finished', (options) =>
+                @drawUnits @stopUnits, options, 'stopUnitMarkers'
                 if @selectedUnits.isSet()
                     @highlightSelectedUnit @selectedUnits.first()
 
@@ -84,6 +89,8 @@ define (require) ->
             @map.on 'click', _.bind(@onMapClicked, @)
             @allMarkers = @getFeatureGroup()
             @allMarkers.addTo @map
+            @stopUnitMarkers = @getFeatureGroup()
+            @stopUnitMarkers.addTo @map
             @allGeometries = L.featureGroup()
             @allGeometries.addTo @map
             @divisionLayer = L.featureGroup()
@@ -187,11 +194,9 @@ define (require) ->
                     @divisionLayer.clearLayers()
                     @drawDivisions @divisions
 
-        drawUnits: (units, options) ->
+        drawUnits: (units, options, layer) ->
             cancelled = false
             options?.cancelToken?.addHandler -> cancelled = true
-
-            @allMarkers.clearLayers()
             @allGeometries.clearLayers()
 
             if units.filters?.bbox?
@@ -199,6 +204,8 @@ define (require) ->
                     return
 
             unitsWithLocation = units.filter (unit) => unit.get('location')?
+            if !layer?
+                unitsWithLocation = unitsWithLocation.filter (unit) => !@isSubwayStation(unit)
             markers = unitsWithLocation.map (unit) => @createMarker(unit, options?.marker)
 
             unitHasGeometry = (unit) ->
@@ -218,7 +225,18 @@ define (require) ->
             if units.length == 1
                 @highlightSelectedUnit units.first()
 
-            @allMarkers.addLayers markers
+            if layer?
+                @[layer].clearLayers()
+                @[layer] = @getFeatureGroup()
+                @[layer].addLayers markers
+                @[layer].addTo @map
+            else
+                @allMarkers.clearLayers()
+                @allMarkers = @getFeatureGroup()
+                @allMarkers.addLayers markers
+                @allMarkers.addTo @map
+            
+
 
         # Prominently highlight the marker whose details are being
         # examined by the user.

src/app-state.coffee
@@ -11,6 +11,7 @@ define (require) ->
             @selectedServices = new models.ServiceList()
             @selectedServiceNodes = new models.ServiceNodeList()
             @units = new models.UnitList null, setComparator: true
+            @stopUnits = new models.UnitList null, setComparator: true
             @selectedUnits = new models.UnitList()
             @selectedEvents = new models.EventList()
             @searchResults = new models.SearchList [], pageSize: appSettings.page_size

src/app.coffee
@@ -290,6 +290,7 @@ define (require) ->
                 searchResults: appModels.searchResults
                 selectedPosition: appModels.selectedPosition
                 selectedDivision: appModels.selectedDivision
+                stopUnits: appModels.stopUnits
                 route: appModels.route
                 divisions: appModels.divisions
                 dataLayers: appModels.dataLayers
@@ -423,6 +424,7 @@ define (require) ->
             "setUnits"
             "setUnit"
             "addUnitsWithinBoundingBoxes"
+            "addStopUnitsWithinBoundingBoxes"
 
             "activateMeasuringTool"
             "deactivateMeasuringTool"

src/control.coffee
@@ -11,6 +11,7 @@ define (require) ->
     renderUnitsByOldServiceId = require 'cs!app/redirect'
 
     GeocodeCleanup = require 'cs!app/geocode-cleanup'
+    { SUBWAY_STATION_SERVICE_ID } = require 'cs!app/transit'
 
     PAGE_SIZE = appSettings.page_size
 
@@ -28,6 +29,7 @@ define (require) ->
             @models = appModels
             # Units currently on the map
             @units = appModels.units
+            @stopUnits = appModels.stopUnits
             # Services and ServiceNodes in the cart
             @selectedServices = appModels.selectedServices
             @serviceNodes = appModels.selectedServiceNodes
@@ -139,6 +141,52 @@ define (require) ->
                     success: (coll, resp, options) =>
                         if unitList.length
                             @units.add unitList.toArray()
+                        unless unitList.fetchNext(opts)
+                            unitList.trigger 'finished',
+                            keepViewport: true
+                unitList.pageSize = PAGE_SIZE
+                unitList.setFilter 'bbox', bboxString
+                layer = p13n.get 'map_background_layer'
+                unitList.setFilter 'bbox_srid', if layer in ['servicemap', 'accessible_map'] then 3067 else 3879
+                if level?
+                    unitList.setFilter 'level', level
+
+                @listenTo unitList, 'finished', =>
+                    getBbox _.rest(bboxStrings)
+                unitList.fetch(opts)
+
+            getBbox(bboxStrings)
+
+        addStopUnitsWithinBoundingBoxes: (bboxStrings, level) ->
+            if level == 'none'
+                return
+            unless level?
+                level = 'customer_service'
+
+            bboxCount = bboxStrings.length
+            if bboxCount > 4
+                null
+                # TODO: handle case.
+
+            @stopUnits.clearFilters()
+
+            getBbox = (bboxStrings) =>
+                # Fetch bboxes sequentially
+                if bboxStrings.length == 0
+                    @stopUnits.setFilter 'bbox', true
+                    @stopUnits.trigger 'finished',
+                        keepViewport: true
+                    return
+                bboxString = _.first bboxStrings
+                unitList = new models.UnitList null, forcedPriority: false
+                opts =
+                    data:
+                        only: UNIT_MINIMAL_ONLY_FIELDS
+                        geometry: 'true'
+                        service: SUBWAY_STATION_SERVICE_ID
+                    success: (coll, resp, options) =>
+                        if unitList.length
+                            @stopUnits.add unitList.toArray()
                         unless unitList.fetchNext(opts)
                             unitList.trigger 'finished',
                             keepViewport: true

src/map-view.coffee
@@ -455,6 +455,9 @@ define (require) ->
             @publicTransitStopsLayer.clearLayers()
             @publicTransitStopsCache = {}
 
+        clearPublicTransitStopUnitsLayer: ->
+            @stopUnitMarkers.clearLayers()
+
         addMapActiveArea: ->
             @map.setActiveArea 'active-area'
             MapView.setMapActiveAreaMaxHeight
@@ -589,17 +592,22 @@ define (require) ->
             if zoom < zoomLimit
                 @clearPublicTransitStopsLayer()
                 @transitStops.reset()
+                @clearPublicTransitStopUnitsLayer()
+                @stopUnits.reset()
 
         updatePublicTransitStops: ->
             if @map.getZoom() < map.MapUtils.getZoomlevelToShowPublicTransitStops()
                 return
             app.request 'requestPublicTransitStops'
 
+        updatePublicTransitStopUnits: (bboxes, level) ->
+            app.request 'addStopUnitsWithinBoundingBoxes', bboxes, level
+
         showAllUnitsAtHighZoom: ->
             if @map.getZoom() < map.MapUtils.getZoomlevelToShowAllMarkers()
                 @previousBoundingBoxes = null
                 return
-            if getIeVersion()
+            if not @stopUnitsOnlyOnZoom() and getIeVersion()
                 return
             if @selectedUnits.isSet() and not @selectedUnits.first().collection?.filters?.bbox?
                 return
@@ -623,7 +631,7 @@ define (require) ->
                 level = @mapOpts.level
                 delete @mapOpts.level
 
-            app.request 'addUnitsWithinBoundingBoxes', bboxes, level
+            @updatePublicTransitStopUnits bboxes, level
 
         print: ->
             @printer.printMap true
@@ -663,5 +671,9 @@ define (require) ->
 
         needsSubwayIcon: (unit) ->
             @isSubwayStation unit
+
+        # Setting for getting stops only on high zoom
+        stopUnitsOnlyOnZoom: ->
+            return true
 
     MapView

commit 0b5e7c8c75b79560281fee9131f96326055266b1

Fix embedded view popups and mouse listeners
Click bug introduced in b301da60ff1fcb581b9ef2a9587337777e1472e8.

src/map-base-view.coffee
@@ -145,6 +145,7 @@ define (require) ->
         postInitialize: ->
             @popups = L.layerGroup()
             @popups.addTo @map
+            @_addMouseoverListeners @allMarkers
             @setInitialView()
             @drawInitialState()
 
@@ -225,14 +226,10 @@ define (require) ->
 
             if layer?
                 @[layer].clearLayers()
-                @[layer] = @getFeatureGroup()
                 @[layer].addLayers markers
-                @[layer].addTo @map
             else
                 @allMarkers.clearLayers()
-                @allMarkers = @getFeatureGroup()
                 @allMarkers.addLayers markers
-                @allMarkers.addTo @map
 
             @removeMarkerTabIndexes()
 
@@ -454,7 +451,6 @@ define (require) ->
                 return new L.LatLngBounds(
                     new L.LatLng(sw.lat - latDiff, sw.lng - lngDiff, true),
                     new L.LatLng(ne.lat + latDiff, ne.lng + lngDiff, true))
-            @_addMouseoverListeners(featureGroup)
             featureGroup
 
         createMarker: (unit, markerOptions) ->